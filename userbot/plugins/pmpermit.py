3d2da850a95d116ce535b3d0d966b6e3a47983fc
Patel Harsh <76835687+Harsh-78@users.noreply.github.com>
2021-04-14 15:08:10 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -1,4 +1,4 @@
-# pmpermit for mafiaBot.....
+# pmpermit for DarkBot.....
 
 import asyncio
 import io
@@ -20,7 +20,7 @@ PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 MAFIAPIC = (
     PMPERMIT_PIC
     if PMPERMIT_PIC
-    else "https://telegra.ph/file/8b086b95491df9f0d4f58.jpg"
+    else "https://telegra.ph/file/503a886dc82e9f5b233a8.jpg"
 )
 PM_WARNS = {}
 PREV_REPLY_MESSAGE = {}
@@ -31,9 +31,9 @@ h1m4n5hu0p = (
     else "**YOU HAVE TRESPASSED TO MY MASTERS INBOX** \n THIS IS ILLEGAL AND REGARDED AS CRIME"
 )
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
-USER_BOT_WARN_ZERO = "**You were spamming my sweet master's inbox, henceforth you have been blocked by my master's MafiaBot.**\n__Now GTFO, i'm busy__"
+USER_BOT_WARN_ZERO = "**You were spamming my sweet master's inbox, henceforth you have been blocked by my master's Darkbot.**\n__Now GTFO, i'm busy__"
 USER_BOT_NO_WARN = (
-    "Hello, This is **MafiaBot Ultra Private Security Protocol⚠️**.\n"
+    "Hello, This is **Darkbot Ultra Private Security Protocol⚠️**.\n"
     f"This is my master {DEFAULTUSER}'s Inbox\n"
     f"\n**{h1m4n5hu0p}**\n\n"
     "To start a valid conversation\n🔱Register Your Request!🔱\nSend `/start` To Register Your Request\nHopefully u will get a reply🔥"
